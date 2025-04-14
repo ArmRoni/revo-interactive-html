60f3aa0db73ad2f5a66aeee9c66866359fb01cd0
roni <ronimia5141@gmail.com>
2025-04-14 19:29:33 +0600

    about us page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,4 +87,16 @@ jQuery(window).scroll(function(){
       },
     });
 
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
   
